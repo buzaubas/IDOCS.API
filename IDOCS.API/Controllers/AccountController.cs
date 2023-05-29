6779059bada56a8c9f1ad3f4c54727d84688f5fd
Admin <Admin@LENOVO>
2023-05-29 18:39:56 +0600

    added documents controller

--- IDOCS.API/Controllers/AccountController.cs
+++ IDOCS.API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using IDOCS.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -8,59 +9,128 @@ namespace IDOCS.API.Controllers
 {
     public class AccountController : Controller
     {
-        // тестовые данные вместо использования базы данных
-        //private List<Person> people = new List<Person>
-        //{
-        //    new Person {Login="admin@gmail.com", Password="12345" },
-        //    new Person { Login="qwerty@gmail.com", Password="55555" }
-        //};
+        private readonly ApplicationDbContext context;
 
-        //[HttpPost("/token")]
-        //public IActionResult Token(string username, string password)
-        //{
-        //    var identity = GetIdentity(username, password);
-        //    if (identity == null)
-        //    {
-        //        return BadRequest(new { errorText = "Invalid username or password." });
-        //    }
+        public AccountController(ApplicationDbContext _context)
+        {
+            context = _context ?? throw new ArgumentNullException(nameof(context));
+        }
 
-        //    var now = DateTime.UtcNow;
-        //    // создаем JWT-токен
-        //    var jwt = new JwtSecurityToken(
-        //            issuer: AuthOptions.ISSUER,
-        //            audience: AuthOptions.AUDIENCE,
-        //            notBefore: now,
-        //            claims: identity.Claims,
-        //            expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
-        //            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
-        //    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+        [HttpPost("/token")]
+        public IActionResult Token(string username, string password)
+        {
+            var identity = GetIdentity(username, password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
 
-        //    var response = new
-        //    {
-        //        access_token = encodedJwt,
-        //        username = identity.Name
-        //    };
+            var now = DateTime.UtcNow;
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
 
-        //    return Json(response);
-        //}
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
 
-        //private ClaimsIdentity GetIdentity(string username, string password)
-        //{
-        //    Person person = people.FirstOrDefault(x => x.Login == username && x.Password == password);
-        //    if (person != null)
-        //    {
-        //        var claims = new List<Claim>
-        //        {
-        //            new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login)
-        //        };
-        //        ClaimsIdentity claimsIdentity =
-        //        new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
-        //            ClaimsIdentity.DefaultRoleClaimType);
-        //        return claimsIdentity;
-        //    }
+            return Json(response);
+        }
 
-        //    // если пользователя не найдено
-        //    return null;
-        //}
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            Person person = context.Persons.FirstOrDefault(x => x.Login == username && x.Password == password);
+            if (person != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login)
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            // если пользователя не найдено
+            return null;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return context.Persons;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Person> Get(int id)
+        {
+            if (id == 0)
+                return BadRequest("Value must be passed to the request");
+            return Ok(context.Persons.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<Person> Post([FromBody] Person person)
+        {
+            try
+            {
+                context.Persons.Add(person);
+                context.SaveChanges();
+                return Ok(person);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut]
+        public ActionResult<Person> Put([FromForm] Person person)
+        {
+            var temp = context.Persons.Find(person.Id);
+            if(temp == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                try
+                {
+                    temp.Login = person.Login;  
+                    temp.Password = person.Password;
+                    context.SaveChanges();
+                    return Ok();
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Person> Delete(int id)
+        {
+            try
+            {
+                var temp =  context.Persons.Find(id);
+                _ = context.Persons.Remove(temp); //?
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
---
+++ IDOCS.API/Controllers/DocumentController.cs
@@ -0,0 +1,120 @@
+﻿using IDOCS.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDOCS.API.Controllers
+{
+    [Authorize]
+    public class DocumentController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public DocumentController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Document> Get()
+        {
+            return context.Documents;
+        }
+
+        [HttpGet("{number}")]
+        public ActionResult<Document> Get(int number)
+        {
+            if (number == 0)
+                return BadRequest("Value must be passed to the request");
+            return Ok(context.Documents.FirstOrDefault(x => x.Number == number));
+        }
+
+        [HttpPost]
+        public ActionResult<Document> Post([FromBody] Document document)
+        {
+            try
+            {
+                context.Documents.Add(document);
+                context.SaveChanges();
+                return Ok(document);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut]
+        public ActionResult<Document> Put([FromForm] Document document)
+        {
+            var temp = context.Documents.Find(document.Number);
+            if (temp == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                try
+                {
+                    temp.Number = document.Number;
+                    temp.Type = document.Type;
+                    temp.CreatedDate = document.CreatedDate;    
+                    temp.CreatedPersonId = document.CreatedPersonId;
+                    temp.Name = document.Name;
+                    temp.Data = document.Data;
+                    temp.ReceiverPersonId = document.ReceiverPersonId; 
+                    context.SaveChanges();
+                    return Ok();
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+        }
+
+        [HttpDelete("{number}")]
+        public ActionResult<Person> Delete(int number)
+        {
+            try
+            {
+                var temp = context.Documents.Find(number);
+                _ = context.Documents.Remove(temp); //?
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("AddDocument")]
+        public async Task<Document> AddDocument(IFormFile file, int number)
+        {
+            Document document = new Document();
+
+            if (file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    document.Data = ms.ToArray();
+                }
+            }
+            document.Number = number;
+
+            context.Documents.Add(document);
+            await context.SaveChangesAsync();
+
+            return document;
+        }
+
+        //[HttpGet("GetContentDocumentById")]
+        //public async Task<IActionResult> GetContentDocumentById(int Id)
+        //{
+        //    var file = context.Documents.Find(Id);
+        //    return File(file.Data, "application/octet-stream", "file.txt");
+        //}
+    }
+}
